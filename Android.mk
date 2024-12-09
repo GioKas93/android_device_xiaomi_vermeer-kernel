TheMysticle/android_device_xiaomi_nuwa-kernel-1

copy headers to KERNEL_OBJ

Android.mk
@@ -1,0 +1,5 @@
+# Kernel headers
+$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr: $(wildcard device/xiaomi/nuwa-kernel/kernel-headers/*)
+	rm -rf $@
+	mkdir -p $@/include
+	cp -a device/xiaomi/nuwa-kernel/kernel-headers/. $@/include

GioKas93/android_device_xiaomi_vermeer-kernel

Android.mk
@@ -1,0 +1,5 @@
+# Kernel headers
+$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr: $(wildcard device/xiaomi/vermeer-kernel/kernel-headers/*)
+	rm -rf $@
+	mkdir -p $@/include
+	cp -a device/xiaomi/vermeer-kernel/kernel-headers/. $@/include